3269405c8ce24515706008161fa8470c1c10a57d
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2021-05-04 16:41:50 +0100

    created the api integration page

--- src/views/settings/integrations/API_integration.jsx
+++ src/views/settings/integrations/API_integration.jsx
@@ -9,17 +9,17 @@ import './API_integration.scss';
 function API_integration() {
     return (
         <MainLayout>
-            <h4>Integrations</h4>
-            <h4>Home / Overview</h4>
+            <h4 className="sam_apih4-1">Integrations</h4>
+            <h4 className="sam_apih4-2">Home / Overview</h4>
             <div className="card sam_apiintegration_container">
 
                 <div className="card-body">
-                    <h3>API Integrations</h3>
+                    <h3 className="sam_apih3-1">API Integrations</h3>
                     <img src={arrowup} className="sam_apiintegration_arrowup" />
-                    <h4>Kindly contact Admin for all developemnt purposes.</h4>
-                    <button type="button" class="btn btn-warning">
-                        Proceed Here
-              </button>
+                    <h4 className="sam_apih4-3">Kindly contact Admin for all developemnt purposes.</h4>
+                    <button type="button" className="btn btn-primary integration_btn" >
+                        <h4 className="sam_apih4-4">Contact Admin</h4>
+                    </button>
 
                 </div>
             </div>
